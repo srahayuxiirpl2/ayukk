7c3b06c93ffc554d5d1d08d59747c29db06b56c9
s.rahayu <cintapertiwiwi@gmail.com>
2022-05-13 10:54:15 +0700

    first commit

---
+++ app/Views/tesview.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Title</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+        </tr>
+        <tr>
+            <td>29</td>
+            <td>Sri Rahayu</td>
+            <td>XI RPL 2</td>
+        </tr>
+    </table>
+</body>
+</html>
